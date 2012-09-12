777e6e42411ea2c56d1dc406f20011d488dfe1e0
Phil Matranga <phil@dmgctrl.com>
2012-09-12 09:43:21 -0400

    added signup page, linked button

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,7 +5,7 @@ describe "UserPages" do
   subject {page}
 
   describe "signup page" do
-  	befor {visit signup_page}
+  	before {visit signup_page}
 
   	it {should have_selector('h1', text: 'Sign up')}
   	it {should have_selector('title', text: full_title('Sign up'))}
