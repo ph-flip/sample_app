33043c19d6f6d0cda4efaa3c85cb15a40659cfcf
Phil Matranga <phil@dmgctrl.com>
2012-09-18 14:05:46 -0400

    styled sign up page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,18 +4,32 @@ describe "UserPages" do
   
   subject { page }
 
-  describe "signup page" do
-  	before { visit signup_path }
-
   	it { should have_selector('h1', text: 'Sign up') }
   	it { should have_selector('title', text: full_title('Sign up')) }
   end
 
-  describe "profile page" do
-  	let(:user) { FactoryGirl.create(:user) }
-  	before { visit user_path(user) }
+  describe "signup page" do
+  	before { visit signup_path }
 
-  	it { should have_selector('h1', text: user.name) }
-  	it { should have_selector('title', text: user.name) }
+  	let(:submit) { "Create my account" }
+
+  	describe "with invalid informatino" do
+  	  it "should not create a user" do
+  	  	execpt { click_button submit }.not_to change(User, :count)
+  	  end
+  	end
+
+  	describe "with valid information" do
+  	  before do
+  	  	fill_in "Name",         with: "Example User"
+  	  	fill_in "Email",	    with: "user@example.com"
+  	  	fill_in "password",     with: "foobar"
+  	  	fill_in "confirmation", with: "foobar"
+  	  end
+
+  	  it "should create a user" do
+  	  	expect { click_button submit }.to change(User, :count).by(1)
+  	  end
+  	end
   end
-end
+$end
