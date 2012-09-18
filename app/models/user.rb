e1b4b7d1c51aaa7b53c1bef2ee59d4b21176120b
Phil Matranga <phil@dmgctrl.com>
2012-09-18 08:27:57 -0400

    worked to 6.3.2 added user password digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,10 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
 
-  validates :name,  presence: true, :length { maximum: 50 }
-  VALIDATE_EMAIL_REGEX = /\A[\W+\-.]+@[a-z\d-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALIDATE_EMAIL_REGEX }
+  before_save { |user| user.email = email.downcase }
+
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  					uniqueness: { case_sensitive: false }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,6 +19,7 @@ describe User do
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should repsond_to(:user_digest) }
 
   it { should be_valid }
 
@@ -35,6 +36,15 @@ describe User do
   describe "when email is not present" do
     before { @user.email = " " }
     it { should_not be_valid }
+  end
+
+  describe "when email address is already taken" do
+  	before do
+  	  user_with_same_email = @user.dup
+  	  user_with_same_email.save
+  	end
+
+  	it { should_not be_valid }
   end
 
   describe "when email format is invalid" do
