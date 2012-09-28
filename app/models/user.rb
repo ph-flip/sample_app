7a6ff8c715a348735ad449f6a486579a6b61f46b
Phil Matranga <phil@dmgctrl.com>
2012-09-28 09:10:06 -0400

    Revert "added admin boolean to db"
    This reverts commit 3ab046a216e72acef8af7ac1d8a1656151f64b64.

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   attr_accessor :password_confirmation
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
-
+  has_many :microposts
   before_save { |user| user.email = email.downcase }
 
   validates :name,  presence: true, length: { maximum: 50 }
