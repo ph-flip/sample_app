3761da873e4a06d7655e8b4227751841b38159dd
Phil Matranga <phil@dmgctrl.com>
2012-09-28 09:25:05 -0400

    added micropost relationship

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,17 @@
-FactoryGirl.define do 
+FactoryGirl.define do
   factory :user do
-  	name "philip matrange"
-  	email "phil@m.com"
-  	password "foobar"
-  	password_confirmation "foobar"
-  end	
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}   
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
+  end
+
+  factory :micropost do
+    content "Lorem ipsum"
+    user
+  end
 end
