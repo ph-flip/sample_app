e152f764e1f542c3b6cd349d3b7df70ef88f12ae
Phil Matranga <phil@dmgctrl.com>
2012-10-01 12:56:55 -0400

    added character count down

---
+++ app/assets/javascripts/microposts.js
@@ -0,0 +1,10 @@
+function charCountDown() {
+	var remaining_char = 140 - jQuery('#micropost_content').val().length;
+	jQuery('.countdown').text(remaining_char + 'characters remaining');
+}
+
+jQuery(document).ready(function($) {
+	charCountDown();
+	$('#micropost_content').change(charCountDown);
+	$('#micropost_content').keyup(charCountDown);
+})
