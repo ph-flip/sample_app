cfbd5383b1ca45d4ebce40c39e14bb51af1baad4
Phil Matranga <phil@dmgctrl.com>
2012-10-04 10:03:01 -0400

    updated charCountDown function to change text colors

--- app/assets/javascripts/microposts.js
+++ app/assets/javascripts/microposts.js
@@ -1,10 +1,16 @@
-function charCountDown() {
-	var remaining_char = 140 - jQuery('#micropost_content').val().length;
-	jQuery('.countdown').text(remaining_char + ' characters remaining');
-}
-
-jQuery(document).ready(function($) {
+$(document).ready(function($) {
 	charCountDown();
 	$('#micropost_content').change(charCountDown);
 	$('#micropost_content').keyup(charCountDown);
 })
+
+function charCountDown() {
+	var remaining_char = 140 - $('#micropost_content').val().length;
+	var char_count = $('.countdown').text(remaining_char + ' characters remaining');
+	
+	if (remaining_char < 0) {
+		char_count.css('color', 'red');
+	} else {
+		char_count.css('color', 'gray');
+	}
+}
